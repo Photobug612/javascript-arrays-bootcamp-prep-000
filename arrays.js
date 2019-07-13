791b1439e5b0da4da439e9013a980226d46d5817
Arlene George <aemg@live.com>
2019-07-13 19:08:10 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -40,7 +40,7 @@ function destructivelyRemoveElementFromBeginningOfArray(array, element){
 }
 
 function removeElementFromBeginningOfArray(array) {
-  var seventhArray = array
+  var seventhArray = array;
   [...seventhArray.slice(0,2),
   ...seventhArray.slice(3) ]
   return seventhArray
